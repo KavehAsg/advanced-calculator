3873d01514d349fae85607c8ef8313c002eb845e
Kaveh Asgari <kaveh.nilkaas@gmail.com>
2022-07-11 15:46:00 +0430

    history html and css completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,17 @@
 const hamMenu = document.querySelector(".ham-menu");
-const hamList = document.querySelector(".ham-list")
+const hamList = document.querySelector(".ham-list");
+const historyBtn = document.querySelector(".history-menubtn");
+const historyContainer = document.querySelector(".history-container")
 
-function hamAction(){
+function hamAction() {
     hamMenu.classList.toggle("active");
-    hamList.classList.toggle("actived");
+    hamList.classList.toggle("active");
 }
 
 
-hamMenu.addEventListener("click" , hamAction);
+hamMenu.addEventListener("click", hamAction);
+historyBtn.addEventListener("click" , () =>{
+    historyContainer.classList.toggle("active")
+    historyBtn.classList.toggle("active")
+
+})
