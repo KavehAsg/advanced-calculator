31d2564a647624118b9e1211c5003510eb344e6b
Kaveh Asgari <kaveh.nilkaas@gmail.com>
2022-07-13 10:10:36 +0430

    .gitignore configuration done

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,27 @@ const hamMenu = document.querySelector(".ham-menu");
 const hamList = document.querySelector(".ham-list");
 const historyBtn = document.querySelector(".history-menubtn");
 const historyContainer = document.querySelector(".history-container")
+const themeBtn = document.querySelector("#theme-toogle");
 
 function hamAction() {
     hamMenu.classList.toggle("active");
     hamList.classList.toggle("active");
 }
 
+function setTeheme (){
+    // themeBtn.checked ? console.log("yep") : console.log("nope");
+    if(themeBtn.checked){
+        document.documentElement.style.setProperty('--theme-color', '#000');
+    } else{
+        document.documentElement.style.setProperty('--theme-color', '#fff');
+
+    }
+}
 
 hamMenu.addEventListener("click", hamAction);
 historyBtn.addEventListener("click" , () =>{
-    historyContainer.classList.toggle("active")
-    historyBtn.classList.toggle("active")
+    historyContainer.classList.toggle("active");
+    historyBtn.classList.toggle("active");
+});
 
-})
+themeBtn.addEventListener("click" , setTeheme);
