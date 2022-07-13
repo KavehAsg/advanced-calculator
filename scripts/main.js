65f6d72471eeb2719a7d1c415c4a01b57b47d27a
Kaveh Asgari <kaveh.nilkaas@gmail.com>
2022-07-13 12:16:13 +0430

    dark mode and main style completed

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,20 @@ function hamAction() {
 }
 
 function setTeheme (){
-    // themeBtn.checked ? console.log("yep") : console.log("nope");
     if(themeBtn.checked){
         document.documentElement.style.setProperty('--theme-color', '#000');
+        document.documentElement.style.setProperty('--primary-color', '#f4511e');
+        document.documentElement.style.setProperty('--secondry-color', '#fff');
+        document.documentElement.style.setProperty('--action-color', '#9e9e9e');
+        document.documentElement.style.setProperty('--back-color', '#9e9e9e');
+        document.querySelector(".theme-btn").innerHTML = `<i class="fa-regular fa-sun"></i>`
     } else{
         document.documentElement.style.setProperty('--theme-color', '#fff');
-
+        document.documentElement.style.setProperty('--primary-color', '#1565c0');
+        document.documentElement.style.setProperty('--secondry-color', '#000');
+        document.documentElement.style.setProperty('--action-color', '#bdbdbd');
+        document.documentElement.style.setProperty('--back-color', '#eeeeee');
+        document.querySelector(".theme-btn").innerHTML = `<i class="fa-regular fa-moon"></i>`
     }
 }
 
